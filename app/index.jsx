dc3120b3d030745ae4e6c9ec8c890f5624d54e80
rahulvijay81 <118264222+rahulvijay81@users.noreply.github.com>
2024-04-22 11:10:17 +0530

    custom button

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,15 @@
 import { ScrollView, View, Image, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Redirect , router } from "expo-router";
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full items-center h-full px-4">
+        <View className="w-full items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -35,8 +38,14 @@ export default function App() {
             exploration with Aora
           </Text>
 
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push('/SignIn')}
+            contentContainerStyle="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
